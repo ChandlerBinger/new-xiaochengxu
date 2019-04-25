d1a61f05f30c151d29886930e36f8489ecd06c50
andy <1064643485@qq.com>
2019-04-25 17:06:25 +0800

    [前两章]

---
+++ pages/logs/logs.js
@@ -0,0 +1,18 @@
+//logs.js
+module.exports = function (value) {
+  return value * 2;
+}
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    logs: []
+  },
+  onLoad: function () {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map(log => {
+        return util.formatTime(new Date(log))
+      })
+    })
+  }
+})
